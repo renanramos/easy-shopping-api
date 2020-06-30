ad11852591103bfc60d4f76515806ba29a5bd2a4
Renan Ramos <renan.ramos@inatel.br>
2020-06-29 23:10:59 -0300

    Finalizado controller para tratamento de exception

--- src/main/java/br/com/renanrramos/easyshopping/controller/exceptionhandler/EasyShoppingExceptionHandlerController.java
+++ src/main/java/br/com/renanrramos/easyshopping/controller/exceptionhandler/EasyShoppingExceptionHandlerController.java
@@ -12,6 +12,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -20,6 +21,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.server.MethodNotAllowedException;
 
+import br.com.renanrramos.easyshopping.constants.ExceptionMessagesConstants;
 import br.com.renanrramos.easyshopping.model.handleerror.ExceptionHandleEntity;
 
 /**
@@ -32,9 +34,20 @@ public class EasyShoppingExceptionHandlerController {
 	@Autowired
 	private MessageSource messageSource;
 	
+	@ExceptionHandler(EmptyResultDataAccessException.class)
+	@ResponseStatus(code = HttpStatus.NOT_FOUND)
+	public ExceptionHandleEntity handleDataNotFound(EmptyResultDataAccessException exception) {
+		return new ExceptionHandleEntity(exception.getMessage());
+	}
 	
+	@ExceptionHandler(MethodNotAllowedException.class)
+	@ResponseStatus(code = HttpStatus.METHOD_NOT_ALLOWED)
 	public ExceptionHandleEntity handleMethodNotAllowed(MethodNotAllowedException exception) {
-		return null;
+		StringBuilder builderMessage = new StringBuilder();
+		builderMessage.append(exception.getStatus());
+		builderMessage.append(" : " + exception.getHttpMethod());
+		builderMessage.append(ExceptionMessagesConstants.METHOD_NOT_ALLOWED_MESSAGE);
+		return new ExceptionHandleEntity(builderMessage.toString());
 	}
 	
 	@ExceptionHandler(MethodArgumentNotValidException.class)
