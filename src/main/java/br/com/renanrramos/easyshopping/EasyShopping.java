96bc9e31ca1b9173388d4e341b666a7ce599282d
Renan Ramos <renanrramossi@gmail.com>
2020-07-04 23:26:03 -0300

    Redirecionamento para pagina incial do Swagger

--- src/main/java/br/com/renanrramos/easyshopping/EasyShopping.java
+++ src/main/java/br/com/renanrramos/easyshopping/EasyShopping.java
@@ -9,11 +9,20 @@ package br.com.renanrramos.easyshopping;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @SpringBootApplication
 @EnableSpringDataWebSupport
+@RestController
 public class EasyShopping {
 	public static void main(String[] args) {
 		SpringApplication.run(EasyShopping.class, args);
+	}
+	
+	@GetMapping
+	public ModelAndView index() {
+		return new ModelAndView("redirect:/swagger-ui.html");
 	}
 }
