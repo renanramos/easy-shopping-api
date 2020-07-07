09cadcc370b1efde27aaf3a8dc7dfebff1a05f93
Renan Ramos <renan.ramos@inatel.br>
2020-07-07 20:33:24 -0300

    ProductCategoryDTO implementado

---
+++ src/main/java/br/com/renanrramos/easyshopping/model/dto/ProductCategoryDTO.java
@@ -0,0 +1,50 @@
+/**------------------------------------------------------------
+ * Project: easy-shopping
+ * 
+ * Creator: renan.ramos - 07/07/2020
+ * ------------------------------------------------------------
+ */
+package br.com.renanrramos.easyshopping.model.dto;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import br.com.renanrramos.easyshopping.model.ProductCategory;
+
+/**
+ * @author renan.ramos
+ *
+ */
+public class ProductCategoryDTO {
+
+	private String name;
+	
+	public ProductCategoryDTO() {
+	}
+
+	public ProductCategoryDTO(ProductCategory productCategory) {
+		this.name = productCategory.getName();
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	private static List<ProductCategoryDTO> converterProductCategoryListToProductCategoryDTOList(List<ProductCategory> productCategory) {
+		return productCategory.stream().map(ProductCategoryDTO::new).collect(Collectors.toList());
+	}
+
+	private static ProductCategoryDTO converterProductCategoryToProductCategoryDTO(ProductCategory productCategory) {
+		return new ProductCategoryDTO(productCategory);
+	}
+
+	@Override
+	public String toString() {
+		return "ProductCategoryDTO [name=" + name + "]";
+	}
+	
+}
