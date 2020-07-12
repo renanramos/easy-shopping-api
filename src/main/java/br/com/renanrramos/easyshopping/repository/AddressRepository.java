74141ea8ac4b55a5ffe0513f3227ae730b16b79a
Renan Ramos <renan.ramos@inatel.br>
2020-07-11 21:21:41 -0300

    Interface AddressRepository

---
+++ src/main/java/br/com/renanrramos/easyshopping/repository/AddressRepository.java
@@ -0,0 +1,19 @@
+/**------------------------------------------------------------
+ * Project: easy-shopping
+ * 
+ * Creator: renan.ramos - 11/07/2020
+ * ------------------------------------------------------------
+ */
+package br.com.renanrramos.easyshopping.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.renanrramos.easyshopping.model.Address;
+
+/**
+ * @author renan.ramos
+ *
+ */
+public interface AddressRepository extends JpaRepository<Address, Long>{
+
+}
