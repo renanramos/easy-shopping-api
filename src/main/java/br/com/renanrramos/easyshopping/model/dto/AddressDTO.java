ca31998a525b666e527698433fb40ea03c8ecf37
Renan Ramos <renan.ramos@inatel.br>
2020-07-11 21:19:38 -0300

    Implementacao da classe AddressDTO

---
+++ src/main/java/br/com/renanrramos/easyshopping/model/dto/AddressDTO.java
@@ -0,0 +1,118 @@
+/**------------------------------------------------------------
+ * Project: easy-shopping
+ * 
+ * Creator: renan.ramos - 11/07/2020
+ * ------------------------------------------------------------
+ */
+package br.com.renanrramos.easyshopping.model.dto;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import br.com.renanrramos.easyshopping.model.Address;
+
+/**
+ * @author renan.ramos
+ *
+ */
+public class AddressDTO {
+
+	private Long id;
+	
+	private String streetName;
+
+	private String district;
+
+	private Long number;
+
+	private String cep;
+
+	private String state;
+	
+	private String customerName;
+	
+	public AddressDTO() {
+	}
+	
+	public AddressDTO(Address address) {
+		this.id = address.getId();
+		this.streetName = address.getStreetName();
+		this.district = address.getDistrict();
+		this.number = address.getNumber();
+		this.cep = address.getCep();
+		this.state = address.getState();
+		this.customerName = address.getCustomer().getName();
+				
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getStreetName() {
+		return streetName;
+	}
+
+	public String getDistrict() {
+		return district;
+	}
+
+	public Long getNumber() {
+		return number;
+	}
+
+	public String getCep() {
+		return cep;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public String getCustomerName() {
+		return customerName;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public void setStreetName(String streetName) {
+		this.streetName = streetName;
+	}
+
+	public void setDistrict(String district) {
+		this.district = district;
+	}
+
+	public void setNumber(Long number) {
+		this.number = number;
+	}
+
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public void setCustomerName(String customerName) {
+		this.customerName = customerName;
+	}
+	
+	public static List<AddressDTO> convertAddressListToAddressDTOList(List<Address> addresses) {
+		return addresses.stream().map(AddressDTO::new).collect(Collectors.toList());
+	}
+	
+	public static AddressDTO convertAddressToAddressDTO(Address address) {
+		return new AddressDTO(address);
+	}
+
+	@Override
+	public String toString() {
+		return "AddressDTO [id=" + id + ", streetName=" + streetName + ", district=" + district + ", number=" + number
+				+ ", cep=" + cep + ", state=" + state + ", customerName=" + customerName + "]";
+	}
+
+}
