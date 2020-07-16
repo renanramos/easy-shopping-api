ba31bca46dea84fd5653d84ebe400dc4e469b171
Renan Ramos <renan.ramos@inatel.br>
2020-07-16 18:47:52 -0300

    Adicionadas mensagens de erro em arquivo de constants separados

---
+++ src/main/java/br/com/renanrramos/easyshopping/constants/messages/ErrorMessagesConstants.java
@@ -0,0 +1,19 @@
+/**------------------------------------------------------------
+ * Project: easy-shopping
+ * 
+ * Creator: renan.ramos - 16/07/2020
+ * ------------------------------------------------------------
+ */
+package br.com.renanrramos.easyshopping.constants.messages;
+
+/**
+ * @author renan.ramos
+ *
+ */
+public class ErrorMessagesConstants {
+
+	public static final String CUSTOMER_NOT_FOUND = "Cliente não localizado/inválido.";
+	public static final String ADDRESS_NOT_FOUND = "Endereço não localizado/inválido.";
+	
+	
+}
