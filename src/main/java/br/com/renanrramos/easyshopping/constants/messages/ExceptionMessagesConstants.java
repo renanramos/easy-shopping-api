5038b268effea71d05bd6270120d69fdede08063
Renan Ramos <renan.ramos@inatel.br>
2020-07-17 17:38:36 -0300

    Arquivo contants contendo mensagens de erro

---
+++ src/main/java/br/com/renanrramos/easyshopping/constants/messages/ExceptionMessagesConstants.java
@@ -0,0 +1,23 @@
+/**------------------------------------------------------------
+ * Project: easy-shopping
+ * 
+ * Creator: renan.ramos - 16/07/2020
+ * ------------------------------------------------------------
+ */
+package br.com.renanrramos.easyshopping.constants.messages;
+
+/**
+ * @author renan.ramos
+ *
+ */
+public class ExceptionMessagesConstants {
+
+	public static final String CUSTOMER_NOT_FOUND = "Cliente não localizado/inválido.";
+	public static final String ADDRESS_NOT_FOUND = "Endereço não localizado/inválido.";
+	public static final String ACCOUNT_NOT_FOUND = "A conta informada não foi localizada";
+	public static final String PRODUCT_CATEGORY_NOT_FOUND = "A categoria informada não foi localizada";
+	public static final String PRODUCT_NOT_FOUND = "O produto informado não foi localizado";
+	public static final String STORE_NOT_FOUND = "A loja informada não foi lcocalizada";
+	
+	
+}
