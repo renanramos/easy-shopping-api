a91d4f6a9f7d4fd285f86546ff1adb83a74a921e
Renan Ramos <renan.ramos@inatel.br>
2020-07-22 22:24:40 -0300

    Novas configuracoes do Swagger

--- src/main/java/br/com/renanrramos/easyshopping/swagger/SwaggerConfig.java
+++ src/main/java/br/com/renanrramos/easyshopping/swagger/SwaggerConfig.java
@@ -8,26 +8,56 @@ package br.com.renanrramos.easyshopping.swagger;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
+import com.google.common.base.Predicate;
+
+import springfox.documentation.RequestHandler;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
-public class SwaggerConfig {
+public class SwaggerConfig extends WebMvcConfigurationSupport{
 
-	private static final String BASE_PACKAGE = "br.com.renanrramos.easyshopping";	
+	private static final String BASE_PACKAGE = "br.com.renanrramos.easyshopping.rest";	
 	
 	@Bean
 	public Docket device() {
 		return new Docket(DocumentationType.SWAGGER_2)
+				.apiInfo(apiInfo())
 				.select()
-				.apis(RequestHandlerSelectors.basePackage(BASE_PACKAGE))
-				.paths(PathSelectors.ant("/**"))
+				.apis(apis())
+				.paths(PathSelectors.any())
 				.build();
+	}
+	
+	private Predicate<RequestHandler> apis() {
+		return RequestHandlerSelectors.basePackage(BASE_PACKAGE);
+	}
+	
+	private ApiInfo apiInfo() {
+		return new ApiInfoBuilder()
+				.title("Easy Shopping API")
+				.description("TCC - PUC Minas 2020")
+				.contact(new Contact("Renan R Ramos","https://github.com/renanramos","renanrramossi@gmail.com"))
+				.build();
+	}
+	
+	@Override
+	protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("swagger-ui.html")
+			.addResourceLocations("classpath:/META-INF/resources/");
+		
+		registry.addResourceHandler("/webjars/**")
+			.addResourceLocations("classpath:/META-INF/resources/webjars/");
 	}
 	
 }
