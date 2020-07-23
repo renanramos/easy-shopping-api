eaf7fd65be2c9cba368fe2d7a7003d37f74b1604
Renan Ramos <renan.ramos@inatel.br>
2020-07-22 22:22:38 -0300

    MainController para redirecionamento de pagina inicial

---
+++ src/main/java/br/com/renanrramos/easyshopping/controller/MainController.java
@@ -0,0 +1,28 @@
+/**------------------------------------------------------------
+ * Project: easy-shopping
+ * 
+ * Creator: renan.ramos - 22/07/2020
+ * ------------------------------------------------------------
+ */
+package br.com.renanrramos.easyshopping.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.view.RedirectView;
+
+/**
+ * @author renan.ramos
+ *
+ */
+@Controller
+@RequestMapping("/")
+public class MainController {
+
+	@GetMapping("/")
+	public RedirectView redirectWithUsingRedirectView(RedirectAttributes attributes) {
+		return new RedirectView("swagger-ui.html");
+	}
+	
+}
