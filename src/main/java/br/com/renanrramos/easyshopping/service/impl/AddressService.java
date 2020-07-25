b1f5a9289eef600591fa90938efaa9f6fa0d62f0
Renan Ramos <renan.ramos@inatel.br>
2020-07-25 19:30:48 -0300

    Remocao de metodo unitilizado

--- src/main/java/br/com/renanrramos/easyshopping/service/impl/AddressService.java
+++ src/main/java/br/com/renanrramos/easyshopping/service/impl/AddressService.java
@@ -50,9 +50,4 @@ public class AddressService implements CommonService<Address> {
 	public void remove(Long addressId) {
 		addressRepository.deleteById(addressId);
 	}
-	
-	public List<Address> getAddressByCustomerId(Long customerId) {
-		return addressRepository.getAddressCustomerId(customerId);
-	}
-
 }
