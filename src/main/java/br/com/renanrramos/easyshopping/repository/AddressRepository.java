34adbb73cb159b733435ba6d9c6f42a2855fc72d
Renan Ramos <renan.ramos@inatel.br>
2020-07-25 19:29:58 -0300

    Remocao de metodos sem uso

--- src/main/java/br/com/renanrramos/easyshopping/repository/AddressRepository.java
+++ src/main/java/br/com/renanrramos/easyshopping/repository/AddressRepository.java
@@ -6,20 +6,12 @@
  */
 package br.com.renanrramos.easyshopping.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-
 import br.com.renanrramos.easyshopping.model.Address;
 
 /**
  * @author renan.ramos
  *
  */
-public interface AddressRepository extends JpaRepository<Address, Long>{
-
-	@Query(" FROM Address a LEFT JOIN Customer c ON (c.id = a.customer) WHERE a.customer = :customerId ")
-	public List<Address> getAddressCustomerId(Long customerId);
-	
+public interface AddressRepository extends JpaRepository<Address, Long>{	
 }
--- src/main/java/br/com/renanrramos/easyshopping/repository/CustomerRepository.java
+++ src/main/java/br/com/renanrramos/easyshopping/repository/CustomerRepository.java
@@ -6,17 +6,9 @@
  */
 package br.com.renanrramos.easyshopping.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-
-import br.com.renanrramos.easyshopping.constants.sql.EasyShoppingConstants;
 import br.com.renanrramos.easyshopping.model.Customer;
 
 public interface CustomerRepository extends JpaRepository<Customer, Long>{
 
-	@Query(EasyShoppingConstants.GET_CUSTOMER_BY_ID)
-	List<Customer> getCustomerById(@Param("customerId") Long customerId);
 }
