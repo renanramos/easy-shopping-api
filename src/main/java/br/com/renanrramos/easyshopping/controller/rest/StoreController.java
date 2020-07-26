e1fafdbed5535663bde2fbf2f273cd0b549ca9f8
Renan Ramos <renan.ramos@inatel.br>
2020-07-26 20:28:37 -0300

    Atualizacao da classe controller

--- src/main/java/br/com/renanrramos/easyshopping/controller/rest/StoreController.java
+++ src/main/java/br/com/renanrramos/easyshopping/controller/rest/StoreController.java
@@ -61,7 +61,7 @@ public class StoreController {
 	@PostMapping
 	@ApiOperation(value = "Save a new store")
 	public ResponseEntity<StoreDTO> saveStore(@Valid @RequestBody StoreForm storeForm, UriComponentsBuilder uriBuilder) {
-		Optional<Company> company = companyService.findById(storeForm.getCompany());
+		Optional<Company> company = companyService.findById(storeForm.getCompanyId());
 		if (company.isPresent()) {
 			Store store = StoreForm.converterStoreFormToStore(storeForm);
 			store.setCompany(company.get());
@@ -73,7 +73,7 @@ public class StoreController {
 		}	
 		return ResponseEntity.badRequest().build();
 	}
-	
+
 	@ResponseBody
 	@GetMapping
 	@ApiOperation(value = "Get all stores")
