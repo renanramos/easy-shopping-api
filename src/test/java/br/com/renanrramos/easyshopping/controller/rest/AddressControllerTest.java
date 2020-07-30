db9460bae8869e599cd57647fb04a98cc306c050
Renan Ramos <renan.ramos@inatel.br>
2020-07-30 10:04:43 -0300

    Correcoes na classe de teste

--- src/test/java/br/com/renanrramos/easyshopping/controller/rest/AddressControllerTest.java
+++ src/test/java/br/com/renanrramos/easyshopping/controller/rest/AddressControllerTest.java
@@ -27,14 +27,11 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-import javax.persistence.EntityNotFoundException;
-
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpHeaders;
