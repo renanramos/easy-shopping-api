5ba0f2ff85587f36b87466e18a5917b7568aceb1
Renan Ramos <renan.ramos@inatel.br>
2020-08-01 22:41:36 -0300

    CreditCardDTO inicio de implementacao

---
+++ src/main/java/br/com/renanrramos/easyshopping/model/dto/CreditCardDTO.java
@@ -0,0 +1,62 @@
+/**------------------------------------------------------------
+ * Project: easy-shopping
+ * 
+ * Creator: renan.ramos - 01/08/2020
+ * ------------------------------------------------------------
+ */
+package br.com.renanrramos.easyshopping.model.dto;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import br.com.renanrramos.easyshopping.model.CreditCard;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * @author renan.ramos
+ *
+ */
+@Getter
+@Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class CreditCardDTO {
+
+	private Long id;
+	
+	private String creditCardNumber;
+
+	private String ownerName;
+
+	private LocalDate validDate;
+
+	private Integer code;
+
+	public CreditCardDTO() {
+		// Intentionally empty
+	}
+
+	public CreditCardDTO(CreditCard creditCard) {
+		this.id = creditCard.getId();
+		this.creditCardNumber = creditCard.getCreditCardNumber();
+		this.ownerName = creditCard.getOwnerName();
+		this.validDate = creditCard.getValidDate();
+		this.code = creditCard.getCode();
+	}
+
+	public static CreditCardDTO converterCreditCardToCreditCardDTO(CreditCard creditCard) {
+		return new CreditCardDTO(creditCard);
+	}
+
+	public static List<CreditCardDTO> converterCreditCardListToCreditCardDTOList(List<CreditCard> creditCards) {
+		return creditCards.stream().map(CreditCardDTO::new).collect(Collectors.toList());
+	}
+
+	@Override
+	public String toString() {
+		return "CreditCardDTO [id=" + id + ", creditCardNumber=" + creditCardNumber + ", ownerName=" + ownerName
+				+ ", validDate=" + validDate + ", code=" + code + "]";
+	}
+}
