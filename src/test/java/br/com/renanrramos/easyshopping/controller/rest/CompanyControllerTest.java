3ad7d3a97a11515ee9b8a88bd4fb8045f7c5cc43
Renan Ramos <renan.ramos@inatel.br>
2020-08-03 18:02:29 -0300

    Melhorias nos testes de CompanyControllerTest

--- src/test/java/br/com/renanrramos/easyshopping/controller/rest/CompanyControllerTest.java
+++ src/test/java/br/com/renanrramos/easyshopping/controller/rest/CompanyControllerTest.java
@@ -11,7 +11,6 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
-import static org.mockito.Mockito.never;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
