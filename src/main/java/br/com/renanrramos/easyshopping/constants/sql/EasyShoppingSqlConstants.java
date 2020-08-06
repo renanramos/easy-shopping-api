eb78dd82894ca674b9ed1206ae79868aeb58b527
Renan Ramos <renan.ramos@inatel.br>
2020-08-06 15:16:36 -0300

    Excluida classe nao utilizada e alterado nome de pacote

---
+++ src/main/java/br/com/renanrramos/easyshopping/constants/sql/EasyShoppingSqlConstants.java
@@ -0,0 +1,17 @@
+/**------------------------------------------------------------
+ * Project: easy-shopping
+ * 
+ * Creator: renan.ramos - 28/06/2020
+ * ------------------------------------------------------------
+ */
+package br.com.renanrramos.easyshopping.constants.sql;
+
+/**
+ * @author renan.ramos
+ *
+ */
+public class EasyShoppingSqlConstants {
+
+	public static final String GET_CUSTOMER_BY_ID = "FROM Customer c WHERE c.id = :customerId";
+	
+}
