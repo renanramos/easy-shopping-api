51e8d7e80cda51679776d41684c166d11afe4966
Renan Ramos <renan.ramos@inatel.br>
2020-08-06 07:49:08 -0300

    Implementacao do metodo de validacao de email

--- src/main/java/br/com/renanrramos/easyshopping/service/impl/UserService.java
+++ src/main/java/br/com/renanrramos/easyshopping/service/impl/UserService.java
@@ -6,9 +6,12 @@
  */
 package br.com.renanrramos.easyshopping.service.impl;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import br.com.renanrramos.easyshopping.model.User;
 import br.com.renanrramos.easyshopping.repository.UserRepository;
 
 /**
@@ -21,4 +24,8 @@ public class UserService {
 	@Autowired
 	private UserRepository userRepository;
 
+	public boolean isValidUserEmail(String email) {
+		List<User> user = userRepository.getUserByEmail(email);
+		return user.isEmpty();
+	}
 }
