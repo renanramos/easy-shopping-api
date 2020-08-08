e8d03e76af7350beb879e3f5016d78b596973940
Renan Ramos <renan.ramos@inatel.br>
2020-08-07 21:41:22 -0300

    Classe com mensagens de validacao de campos

---
+++ src/main/java/br/com/renanrramos/easyshopping/constants/messages/ValidationMessagesConstants.java
@@ -0,0 +1,21 @@
+/**------------------------------------------------------------
+ * Project: easy-shopping
+ * 
+ * Creator: renan.ramos - 07/08/2020
+ * ------------------------------------------------------------
+ */
+package br.com.renanrramos.easyshopping.constants.messages;
+
+/**
+ * @author renan.ramos
+ *
+ */
+public class ValidationMessagesConstants {
+
+	private ValidationMessagesConstants() {
+		// Intentionally empty
+	}
+
+	public static final String EMPTY_FIELD = "O campo não pode estar vazio.";
+	public static final String INVALID_REGISTERED_NUMBER_FORMAT = "O CPF/CNPJ não está no formato correto.";
+}
