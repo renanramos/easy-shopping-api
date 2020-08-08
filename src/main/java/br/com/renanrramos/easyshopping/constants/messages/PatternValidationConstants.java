9380a73bd3d219d1afa420325f645a7e8bc11963
Renan Ramos <renan.ramos@inatel.br>
2020-08-07 21:40:56 -0300

    Classe com constants relacionados a patterns

---
+++ src/main/java/br/com/renanrramos/easyshopping/constants/messages/PatternValidationConstants.java
@@ -0,0 +1,21 @@
+/**------------------------------------------------------------
+ * Project: easy-shopping
+ * 
+ * Creator: renan.ramos - 07/08/2020
+ * ------------------------------------------------------------
+ */
+package br.com.renanrramos.easyshopping.constants.messages;
+
+/**
+ * @author renan.ramos
+ *
+ */
+public class PatternValidationConstants {
+
+	public PatternValidationConstants() {
+		// Intentionally empty
+	}
+
+	public static final String CPF_PATTERN = "^[0-9]{3}.[0-9]{3}.[0-9]{3}-[0-9]{2}$";
+	public static final String REGISTERED_NUMBER_PATTERN = "^[0-9]{2}[.][0-9]{3}[.][0-9]{4}[-][0-9]{2}$";
+}
