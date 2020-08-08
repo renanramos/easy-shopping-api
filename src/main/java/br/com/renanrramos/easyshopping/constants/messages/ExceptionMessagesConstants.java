b63cb1cd0fbfc3851a9c5b7fcea8fffcdba48f2c
Renan Ramos <renan.ramos@inatel.br>
2020-08-08 00:26:31 -0300

    Mensagens de excecao implementadas

--- src/main/java/br/com/renanrramos/easyshopping/constants/messages/ExceptionMessagesConstants.java
+++ src/main/java/br/com/renanrramos/easyshopping/constants/messages/ExceptionMessagesConstants.java
@@ -12,8 +12,9 @@ package br.com.renanrramos.easyshopping.constants.messages;
  */
 public class ExceptionMessagesConstants {
 
-	public static final String CUSTOMER_NOT_FOUND = "Cliente não localizado/inválido.";
-	public static final String ADDRESS_NOT_FOUND = "Endereço não localizado/inválido.";
+	public static final String CUSTOMER_NOT_FOUND = "Cliente não localizado.";
+	public static final String COMPANY_NOT_FOUND = "Empresa não localizada.";
+	public static final String ADDRESS_NOT_FOUND = "Endereço não localizado.";
 	public static final String ACCOUNT_NOT_FOUND = "A conta informada não foi localizada.";
 	public static final String PRODUCT_CATEGORY_NOT_FOUND = "A categoria informada não foi localizada.";
 	public static final String PRODUCT_NOT_FOUND = "O produto informado não foi localizado.";
@@ -21,6 +22,11 @@ public class ExceptionMessagesConstants {
 	public static final String CREDIT_CARD_NOT_FOUND = "Cartão de crédito não localizado.";
 	
 	public static final String INVALID_ID = "O id informado é inválido.";
+	public static final String CUSTOMER_ID_NOT_FOUND_ON_REQUEST = "Id do cliente é obrigatório.";
+	public static final String COMPANY_ID_NOT_FOUND_ON_REQUEST = "Id da empresa é obrigatório.";
+	public static final String PRODUCT_CATEGORY_ID_NOT_FOUND_ON_REQUEST = "Id da categoria do produto é obrigatório.";
+	public static final String PRODUCT_ID_NOT_FOUND_ON_REQUEST = "Id do produto é obrigatório.";
+	public static final String STORE_ID_NOT_FOUND_ON_REQUEST = "Id da loja é obrigatório.";
 
 	public static final String EMAIL_ALREADY_EXIST = "O email informado já é utilizado.";
 	public static final String CPF_ALREADY_EXIST = "O CPF informado já está cadastrado.";
