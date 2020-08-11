0b8e8d0f2aa46a5d48316f4ff1ccd45e1673562e
Renan Ramos <renan.ramos@inatel.br>
2020-08-11 19:21:06 -0300

    Atualizado controller com nova implementacao de login para geracao de token

--- src/main/java/br/com/renanrramos/easyshopping/controller/rest/UserController.java
+++ src/main/java/br/com/renanrramos/easyshopping/controller/rest/UserController.java
@@ -6,14 +6,16 @@
  */
 package br.com.renanrramos.easyshopping.controller.rest;
 
-import java.net.URI;
-import java.util.Optional;
-
 import javax.transaction.Transactional;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -21,10 +23,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.renanrramos.easyshopping.model.User;
-import br.com.renanrramos.easyshopping.model.dto.UserDTO;
+import br.com.renanrramos.easyshopping.config.JwtTokenUtil;
+import br.com.renanrramos.easyshopping.constants.messages.ExceptionMessagesConstants;
+import br.com.renanrramos.easyshopping.exception.EasyShoppingException;
 import br.com.renanrramos.easyshopping.model.form.LoginForm;
-import br.com.renanrramos.easyshopping.service.impl.UserService;
+import br.com.renanrramos.easyshopping.model.jwt.JwtResponse;
+import br.com.renanrramos.easyshopping.service.jwt.JwtUserDetailsService;
 import io.swagger.annotations.Api;
 
 /**
@@ -38,18 +42,31 @@ import io.swagger.annotations.Api;
 public class UserController {
 
 	@Autowired
-	private UserService userService;
+	private AuthenticationManager authenticationManager;
 
-	private URI uri;
+	@Autowired
+	private JwtTokenUtil jwtTokenUtil;
+
+	@Autowired
+	private JwtUserDetailsService userDetailsService;
 
 	@ResponseBody
 	@PostMapping(path = "/login")
 	@Transactional
-	public ResponseEntity<UserDTO> login(@Valid @RequestBody LoginForm loginForm) {
-		Optional<User> userOptional = userService.login(loginForm);
-		if (userOptional.isPresent()) {
-			return ResponseEntity.ok(UserDTO.converterUserToUserDTO(userOptional.get()));			
+	public ResponseEntity<Object> login(@Valid @RequestBody LoginForm loginForm) throws EasyShoppingException {
+		authentication(loginForm.getEmail(), loginForm.getPassword());
+		final UserDetails userDetails = userDetailsService.loadUserByUsername(loginForm.getEmail());
+		final String token = jwtTokenUtil.generateToken(userDetails);
+		return ResponseEntity.ok(new JwtResponse(token));
+	}
+
+	private void authentication(String email, String password) throws EasyShoppingException {
+		try {
+			authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(email, password));			
+		} catch (DisabledException ex) {
+			throw new EasyShoppingException(ExceptionMessagesConstants.USER_DISABLED);
+		} catch (BadCredentialsException ex) {
+			throw new EasyShoppingException(ExceptionMessagesConstants.INVALID_CREDENTIALS);
 		}
-		return ResponseEntity.notFound().build();
 	}
 }
