b41267342bfc76519b20b3ece5a3162a4f5e482f
Renan Ramos <renan.ramos@inatel.br>
2020-08-10 21:31:06 -0300

    Remocao de imports

--- src/test/java/br/com/renanrramos/easyshopping/controller/rest/CustomerControllerTest.java
+++ src/test/java/br/com/renanrramos/easyshopping/controller/rest/CustomerControllerTest.java
@@ -32,7 +32,6 @@ import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
