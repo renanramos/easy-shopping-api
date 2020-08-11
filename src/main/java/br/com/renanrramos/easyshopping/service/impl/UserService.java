53c0563697bdfd9b4a6814ca93340be34afb51c5
Renan Ramos <renan.ramos@inatel.br>
2020-08-11 07:49:28 -0300

    Implementacao do metodo de login na camada Service

--- src/main/java/br/com/renanrramos/easyshopping/service/impl/UserService.java
+++ src/main/java/br/com/renanrramos/easyshopping/service/impl/UserService.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.com.renanrramos.easyshopping.model.User;
+import br.com.renanrramos.easyshopping.model.form.LoginForm;
 import br.com.renanrramos.easyshopping.repository.UserRepository;
 
 /**
@@ -27,5 +28,9 @@ public class UserService {
 	public boolean isUserEmailInvalid(String email) {
 		User user = userRepository.findTopUserByEmail(email);
 		return Optional.ofNullable(user).isPresent();
+	}
+
+	public Optional<User> login(LoginForm loginForm) {
+		return userRepository.findTopUserByEmailAndPassword(loginForm.getEmail(), loginForm.getPassword());
 	}
 }
