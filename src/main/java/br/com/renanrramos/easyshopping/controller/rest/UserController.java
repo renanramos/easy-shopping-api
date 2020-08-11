9c25a91eabdab5c6c1620b4a2369ad18090543e7
Renan Ramos <renan.ramos@inatel.br>
2020-08-11 07:49:59 -0300

    Implementacao inicial de login em UserController

--- src/main/java/br/com/renanrramos/easyshopping/controller/rest/UserController.java
+++ src/main/java/br/com/renanrramos/easyshopping/controller/rest/UserController.java
@@ -7,8 +7,10 @@
 package br.com.renanrramos.easyshopping.controller.rest;
 
 import java.net.URI;
+import java.util.Optional;
 
 import javax.transaction.Transactional;
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -18,14 +20,12 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.util.UriComponentsBuilder;
 
 import br.com.renanrramos.easyshopping.model.User;
 import br.com.renanrramos.easyshopping.model.dto.UserDTO;
-import br.com.renanrramos.easyshopping.model.form.UserForm;
+import br.com.renanrramos.easyshopping.model.form.LoginForm;
 import br.com.renanrramos.easyshopping.service.impl.UserService;
 import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
 
 /**
  * @author renan.ramos
@@ -42,4 +42,14 @@ public class UserController {
 
 	private URI uri;
 
+	@ResponseBody
+	@PostMapping(path = "/login")
+	@Transactional
+	public ResponseEntity<UserDTO> login(@Valid @RequestBody LoginForm loginForm) {
+		Optional<User> userOptional = userService.login(loginForm);
+		if (userOptional.isPresent()) {
+			return ResponseEntity.ok(UserDTO.converterUserToUserDTO(userOptional.get()));			
+		}
+		return ResponseEntity.notFound().build();
+	}
 }
