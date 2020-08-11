583832150c7b3fd7e9e36ca29f5b46bf1c4e42b9
Renan Ramos <renan.ramos@inatel.br>
2020-08-10 21:21:28 -0300

    Alterada classe pai com metodos de consulta recebendo parametro de paginacao

--- src/main/java/br/com/renanrramos/easyshopping/service/CommonService.java
+++ src/main/java/br/com/renanrramos/easyshopping/service/CommonService.java
@@ -9,6 +9,8 @@ package br.com.renanrramos.easyshopping.service;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.domain.Pageable;
+
 /**
  * @author renan.ramos
  *
@@ -16,14 +18,14 @@ import java.util.Optional;
 public interface CommonService<T> {
 
 	public T save(T entity);
-	
-	public List<T> findAll();
-	
+
+	public List<T> findAll(Pageable page);
+
 	public Optional<T> findById(Long entityId);
 	
 	public T update(T entity);
 	
 	public void remove(Long entityId);
 
-	public List<T> findAllPageable(Integer pageNumber, Integer pageSize, String sort, Long id);
+	public List<T> findAllPageable(Pageable page, Long id);
 }
