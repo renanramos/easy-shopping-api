6cde2064ef14e96931a744942dea150cc1ee8870
Renan Ramos <renan.ramos@inatel.br>
2020-08-11 19:22:42 -0300

    Classe model response de token

---
+++ src/main/java/br/com/renanrramos/easyshopping/model/jwt/JwtResponse.java
@@ -0,0 +1,31 @@
+/**------------------------------------------------------------
+ * Project: easy-shopping
+ * 
+ * Creator: renan.ramos - 11/08/2020
+ * ------------------------------------------------------------
+ */
+package br.com.renanrramos.easyshopping.model.jwt;
+
+import java.io.Serializable;
+
+import lombok.Getter;
+
+/**
+ * @author renan.ramos
+ *
+ */
+@Getter
+public class JwtResponse implements Serializable{
+
+	private static final long serialVersionUID = 1631739111032561633L;
+
+	private String jwtToken;
+
+	public JwtResponse() {
+		// Intentionally empty
+	}
+
+	public JwtResponse(String jwtToken) {
+		this.jwtToken = jwtToken;
+	}
+}
