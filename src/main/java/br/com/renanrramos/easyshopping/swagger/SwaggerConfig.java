01a921f398826817320ce15b3ad0a19198cf6748
Renan Ramos <renan.ramos@inatel.br>
2020-08-11 21:50:26 -0300

    Incluidas configuracoes para o funcionamento do swagger

--- src/main/java/br/com/renanrramos/easyshopping/swagger/SwaggerConfig.java
+++ src/main/java/br/com/renanrramos/easyshopping/swagger/SwaggerConfig.java
@@ -6,58 +6,84 @@
  */
 package br.com.renanrramos.easyshopping.swagger;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 import com.google.common.base.Predicate;
+import com.google.common.collect.Lists;
 
 import springfox.documentation.RequestHandler;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
 import springfox.documentation.service.Contact;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
-public class SwaggerConfig extends WebMvcConfigurationSupport{
+public class SwaggerConfig extends WebMvcConfigurationSupport {
 
-	private static final String BASE_PACKAGE = "br.com.renanrramos.easyshopping.controller.rest";	
-	
+	private static final String BASE_PACKAGE = "br.com.renanrramos.easyshopping.controller.rest";
+
+	public static final String AUTHORIZATION_HEADER = "Authorization";
+
+	public static final String DEFAULT_INCLUDE_PATTERN = "/api/.*";
+
 	@Bean
 	public Docket device() {
 		return new Docket(DocumentationType.SWAGGER_2)
 				.apiInfo(apiInfo())
+				.securityContexts(Lists.newArrayList(securityContext()))
+	            .securitySchemes(Lists.newArrayList(apiKey()))
+				.pathMapping("/")
+				.forCodeGeneration(true)
 				.select()
 				.apis(apis())
 				.paths(PathSelectors.any())
 				.build();
 	}
-	
+
+	private ApiKey apiKey() {
+		return new ApiKey("JWT", AUTHORIZATION_HEADER, "header");
+	}
+
+	private SecurityContext securityContext() {
+		return SecurityContext.builder().securityReferences(defaultAuth())
+				.forPaths(PathSelectors.regex(DEFAULT_INCLUDE_PATTERN)).build();
+	}
+
 	private Predicate<RequestHandler> apis() {
 		return RequestHandlerSelectors.basePackage(BASE_PACKAGE);
 	}
-	
+
 	private ApiInfo apiInfo() {
-		return new ApiInfoBuilder()
-				.title("Easy Shopping API")
-				.description("TCC - PUC Minas 2020")
-				.contact(new Contact("Renan R Ramos","https://github.com/renanramos","renanrramossi@gmail.com"))
+		return new ApiInfoBuilder().title("Easy Shopping API").description("TCC - PUC Minas 2020")
+				.contact(new Contact("Renan R Ramos", "https://github.com/renanramos", "renanrramossi@gmail.com"))
 				.build();
 	}
-	
+
 	@Override
 	protected void addResourceHandlers(ResourceHandlerRegistry registry) {
-		registry.addResourceHandler("swagger-ui.html")
-			.addResourceLocations("classpath:/META-INF/resources/");
-		
-		registry.addResourceHandler("/webjars/**")
-			.addResourceLocations("classpath:/META-INF/resources/webjars/");
+		registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+
+		registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
 	}
-	
+
+	List<SecurityReference> defaultAuth() {
+		AuthorizationScope authorizationScope = new AuthorizationScope("/api/users/login", "administrators");
+		AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+		authorizationScopes[0] = authorizationScope;
+		return Lists.newArrayList(new SecurityReference("JWT", authorizationScopes));
+	}
 }
