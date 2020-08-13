633a4877f05f97f6721aee3bffef41d9483c6f8e
Renan Ramos <renan.ramos@inatel.br>
2020-08-12 21:05:58 -0300

    Pesquisa por email e retorna um User

--- src/main/java/br/com/renanrramos/easyshopping/service/jwt/JwtUserDetailsService.java
+++ src/main/java/br/com/renanrramos/easyshopping/service/jwt/JwtUserDetailsService.java
@@ -6,15 +6,20 @@
  */
 package br.com.renanrramos.easyshopping.service.jwt;
 
+import java.util.ArrayList;
+import java.util.Optional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import br.com.renanrramos.easyshopping.constants.messages.ExceptionMessagesConstants;
-import br.com.renanrramos.easyshopping.model.User;
-import br.com.renanrramos.easyshopping.repository.UserRepository;
+import br.com.renanrramos.easyshopping.service.impl.UserService;
 
 /**
  * @author renan.ramos
@@ -23,16 +28,25 @@ import br.com.renanrramos.easyshopping.repository.UserRepository;
 @Service
 public class JwtUserDetailsService implements UserDetailsService{
 
+	private static final Logger LOG = LoggerFactory.getLogger(JwtUserDetailsService.class);
+	
 	@Autowired
-	private UserRepository userRepository;
+	private UserService userService;
 	
 	@Override
-	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-		User user = userRepository.findTopUserByEmail(email);
-		if(user == null) {
+	public UserDetails loadUserByUsername(String email) {
+
+		LOG.info("Email selecionado: {} ", email);
+		
+		Optional<br.com.renanrramos.easyshopping.model.User> userOptional = userService.findUserByEmail(email);
+		if(!userOptional.isPresent()) {
+			LOG.error("Email não encontrado: {} ", email);
 			throw new UsernameNotFoundException(ExceptionMessagesConstants.INVALID_CREDENTIALS);
 		}
-		return null;
+
+		br.com.renanrramos.easyshopping.model.User userEasyShopping = userOptional.get();
+
+		return new User(email, userEasyShopping.getName(), new ArrayList<>());
 	}
 
 }
