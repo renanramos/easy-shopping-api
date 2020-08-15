30276492506e406273b7b1f16a4cd9a09043d09f
Renan Rodrigues Ramos <renanrramossi@gmail.com>
2020-08-15 01:44:06 -0300

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,2 @@
-CREATE SCHEMA if not exists `easy-shopping`;
-
 INSERT INTO `easy-shopping`.user(id, email, name, password, profile) VALUES (1, 'admin@mail.com', 'Administrator', '$2a$10$fBVfqooCFheIBubwGCNIEuqVV66gGEhLqxa/dW.cLT3CLPTuXTlX.', 'ADMINISTRATOR');
 INSERT INTO `easy-shopping`.administrator(id) VALUES(1);
