80dd44d43680b77ccd86f1fcc339594627719827
Renan Ramos <renan.ramos@inatel.br>
2020-08-15 01:28:54 -0300

    Cria usuario administrador na inicializacao

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,4 @@
 CREATE SCHEMA if not exists `easy-shopping`;
 
-INSERT INTO `easy-shopping`.administrator(id, name, profile) VALUES(1, 'Administrador', 'ADMINISTRATOR');
+INSERT INTO `easy-shopping`.user(id, email, name, password, profile) VALUES (1, 'admin@mail.com', 'Administrator', '$2a$10$fBVfqooCFheIBubwGCNIEuqVV66gGEhLqxa/dW.cLT3CLPTuXTlX.', 'ADMINISTRATOR');
+INSERT INTO `easy-shopping`.administrator(id) VALUES(1);
