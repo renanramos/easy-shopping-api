79a258826b3ae978fbaefd50e454831d7fa19fd5
Renan Ramos <renan.ramos@inatel.br>
2020-08-15 21:16:35 -0300

    Alteracao de sript sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,2 @@
-CREATE SCHEMA if not exists `easy-shopping`;
-
-INSERT INTO `easy-shopping`.user(id, email, name, password, profile) VALUES (1, 'admin@mail.com', 'Administrator', '$2a$10$fBVfqooCFheIBubwGCNIEuqVV66gGEhLqxa/dW.cLT3CLPTuXTlX.', 'ADMINISTRATOR');
-INSERT INTO `easy-shopping`.administrator(id) VALUES(1);
+INSERT INTO user(id, email, name, password, profile) VALUES (1, 'admin@mail.com', 'Administrator', '$2a$10$fBVfqooCFheIBubwGCNIEuqVV66gGEhLqxa/dW.cLT3CLPTuXTlX.', 'ADMINISTRATOR');
+INSERT INTO administrator(id) VALUES(1);
