e42487d1864be56891b153f8fbd74f2792befb4c
Renan Ramos <renan.ramos@inatel.br>
2020-08-17 21:38:27 -0300

    Implementacao de metodo para exclusao por id

--- src/main/java/br/com/renanrramos/easyshopping/repository/CreditCardRepositoy.java
+++ src/main/java/br/com/renanrramos/easyshopping/repository/CreditCardRepositoy.java
@@ -6,8 +6,12 @@
  */
 package br.com.renanrramos.easyshopping.repository;
 
+import javax.transaction.Transactional;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 import br.com.renanrramos.easyshopping.model.CreditCard;
@@ -19,4 +23,9 @@ import br.com.renanrramos.easyshopping.model.CreditCard;
 public interface CreditCardRepositoy extends PagingAndSortingRepository<CreditCard, Long>{
 	
 	Page<CreditCard> findCreditCardByCustomerId(Pageable page, Long customerId);
+
+	@Transactional
+	@Modifying
+	@Query("DELETE FROM CreditCard WHERE ID = :creditCardId")
+	public void removeById(Long creditCardId);
 }
