04db95a5637d56a482afdd6f749237be435d14a4
Renan Ramos <renan.ramos@inatel.br>
2020-08-18 15:50:18 -0300

    Implementacao de metodos para validacao de senha e email

--- src/main/java/br/com/renanrramos/easyshopping/service/jwt/JwtUserDetailsService.java
+++ src/main/java/br/com/renanrramos/easyshopping/service/jwt/JwtUserDetailsService.java
@@ -6,20 +6,21 @@
  */
 package br.com.renanrramos.easyshopping.service.jwt;
 
-import java.util.List;
+import java.util.Optional;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import br.com.renanrramos.easyshopping.config.util.EasyShoppingUtils;
+import br.com.renanrramos.easyshopping.config.util.JwtTokenUtil;
 import br.com.renanrramos.easyshopping.constants.messages.ExceptionMessagesConstants;
+import br.com.renanrramos.easyshopping.model.form.LoginForm;
 import br.com.renanrramos.easyshopping.service.impl.UserService;
 
 /**
@@ -27,20 +28,41 @@ import br.com.renanrramos.easyshopping.service.impl.UserService;
  *
  */
 @Service
-public class JwtUserDetailsService implements UserDetailsService{
+public class JwtUserDetailsService implements UserDetailsService {
 
 	private Logger LOG = LoggerFactory.getLogger(JwtUserDetailsService.class);
 	
 	@Autowired
 	private UserService userService;
 
+	@Autowired
+	private EasyShoppingUtils utils;
+
+	@Autowired
+	private JwtTokenUtil jwtTokenUtil;
+
 	@Override
 	public UserDetails loadUserByUsername(String email) {
-		br.com.renanrramos.easyshopping.model.User user = userService.findUserByEmail(email)
+		br.com.renanrramos.easyshopping.model.User user = userService.findUserByEmailAndPassword(email)
 				.orElseThrow(() -> new UsernameNotFoundException(ExceptionMessagesConstants.USER_NOT_FOUND));
+		return new User(email, user.getName(), jwtTokenUtil.createUserAuthorityList(user));
+	}
+
+	public br.com.renanrramos.easyshopping.model.User loadUserByUsernameAndPassword(LoginForm loginForm) {
+		Optional<br.com.renanrramos.easyshopping.model.User> userOptional = userService.findUserByEmailAndPassword(loginForm.getEmail());
+
+		if (!userOptional.isPresent()) {
+			throw new UsernameNotFoundException(ExceptionMessagesConstants.INVALID_CREDENTIALS);
+		}
+
+		br.com.renanrramos.easyshopping.model.User user = userOptional.get();
+
+		if (!utils.verifyPassword(loginForm.getPassword(), user.getPassword())) {
+			throw new UsernameNotFoundException(ExceptionMessagesConstants.INVALID_CREDENTIALS);
+		}
+
 		LOG.info("User role : {}", user.getProfile().getRole());
-		List<GrantedAuthority> authorityList = AuthorityUtils.createAuthorityList(user.getProfile().getRole());
-		return new User(email, user.getName(), authorityList);
+		return user;
 	}
 
 }
