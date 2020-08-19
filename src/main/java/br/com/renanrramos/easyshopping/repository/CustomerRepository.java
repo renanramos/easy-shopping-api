2d2a521421b548cf7a8e6561cad65fc70ec1d31b
Renan Ramos <renan.ramos@inatel.br>
2020-08-19 17:09:35 -0300

    Declaracao de metodo para consulta de customer por nome

--- src/main/java/br/com/renanrramos/easyshopping/repository/CustomerRepository.java
+++ src/main/java/br/com/renanrramos/easyshopping/repository/CustomerRepository.java
@@ -6,6 +6,8 @@
  */
 package br.com.renanrramos.easyshopping.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 import br.com.renanrramos.easyshopping.model.Customer;
@@ -14,4 +16,5 @@ public interface CustomerRepository extends PagingAndSortingRepository<Customer,
 
 	Customer findTopCustomerByCpf(String cpf);
 
+	Page<Customer> findCustomerByNameContaining(Pageable page, String name);
 }
