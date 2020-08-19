108d058c6e65efdf38f9fddeccb373fff5662e75
Renan Ramos <renan.ramos@inatel.br>
2020-08-19 17:01:46 -0300

    Declaracao de metodo para consulta de empresa por nome

--- src/main/java/br/com/renanrramos/easyshopping/repository/CompanyRepository.java
+++ src/main/java/br/com/renanrramos/easyshopping/repository/CompanyRepository.java
@@ -6,6 +6,8 @@
  */
 package br.com.renanrramos.easyshopping.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 import br.com.renanrramos.easyshopping.model.Company;
@@ -17,4 +19,6 @@ import br.com.renanrramos.easyshopping.model.Company;
 public interface CompanyRepository extends PagingAndSortingRepository<Company, Long>{
 
 	Company findTopCompanyByRegisteredNumber(String registeredNumber);
+
+	Page<Company> findCompanyByNameContaining(Pageable page, String name);
 }
