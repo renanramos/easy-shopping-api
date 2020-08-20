0c4b0c590fbae4c7ddd13206941a37aceb3d0cf4
Renan Ramos <renan.ramos@inatel.br>
2020-08-19 22:52:18 -0300

    Correcao de data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,2 @@
-DROP TABLE IF EXISTS customer CASCADE;
-DROP TABLE IF EXISTS company CASCADE;
-DROP TABLE IF EXISTS administrator CASCADE;
-DROP TABLE IF EXISTS user CASCADE;
 INSERT INTO user(id, email, name, password, profile) VALUES (1, 'admin@mail.com', 'Administrator', '$2a$10$fBVfqooCFheIBubwGCNIEuqVV66gGEhLqxa/dW.cLT3CLPTuXTlX.', 'ADMINISTRATOR');
 INSERT INTO administrator(id) VALUES(1);
