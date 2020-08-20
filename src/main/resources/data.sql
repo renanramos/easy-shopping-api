8304813c8144f7ba393727d18926dbba9bc04d9d
Renan Ramos <renan.ramos@inatel.br>
2020-08-19 22:17:59 -0300

    Alterada instrucao quando build do projeto for feito

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,3 @@
+DROP TABLE IF EXISTS user;
 INSERT INTO user(email, name, password, profile) VALUES ('admin@mail.com', 'Administrator', '$2a$10$fBVfqooCFheIBubwGCNIEuqVV66gGEhLqxa/dW.cLT3CLPTuXTlX.', 'ADMINISTRATOR');
 INSERT INTO administrator(id) VALUES(1);
