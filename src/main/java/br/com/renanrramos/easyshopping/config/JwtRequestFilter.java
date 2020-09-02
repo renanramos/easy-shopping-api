2ab24a24dfbecc6ea1235a43c508e96250fe950d
Renan Ramos <renan.ramos@inatel.br>
2020-09-01 22:02:51 -0300

    Adicionado metodo PATCH como metodo permitido

--- src/main/java/br/com/renanrramos/easyshopping/config/JwtRequestFilter.java
+++ src/main/java/br/com/renanrramos/easyshopping/config/JwtRequestFilter.java
@@ -75,7 +75,7 @@ public class JwtRequestFilter extends OncePerRequestFilter{
 	private HttpServletResponse setResponseHeaders(HttpServletRequest request, HttpServletResponse response) {
 		response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
 	    response.setHeader("Access-Control-Allow-Credentials", "true");
-	    response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
+	    response.setHeader("Access-Control-Allow-Methods", "POST, PATCH, PUT, GET, OPTIONS, DELETE");
 	    response.setHeader("Access-Control-Max-Age", "3600");
 	    response.setHeader("Access-Control-Allow-Headers", "*");
 		return response;
