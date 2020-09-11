37147e6b24fc9d8594492670f337129b1944a4f8
Renan Ramos <renan.ramos@inatel.br>
2020-09-11 10:45:25 -0300

    Inicializacao de dados

---
+++ src/main/resources/data.sql
@@ -0,0 +1,9 @@
+INSERT INTO USER(id, name, email, password, profile) values(1, 'Administrator', 'admin@mail.com', '$2a$10$fBVfqooCFheIBubwGCNIEuqVV66gGEhLqxa/dW.cLT3CLPTuXTlX.', 'ADMINISTRATOR');
+INSERT INTO administrator(id) values(1);
+
+INSERT INTO product_category(name) values('Móveis');
+INSERT INTO product_category(name) values('Eletrodomésticos');
+INSERT INTO product_category(name) values('Tecnologia');
+INSERT INTO product_category(name) values('Brinquedos');
+INSERT INTO product_category(name) values('Ferramentas');
+INSERT INTO product_category(name) values('Moda');
