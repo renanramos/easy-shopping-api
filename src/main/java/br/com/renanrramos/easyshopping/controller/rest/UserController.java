228676aea33ffd85d878430b67fbbdcf38c9bdfe
Renan Ramos <renan.ramos@inatel.br>
2020-09-12 23:05:46 -0300

    Mensagem de resposta quando usuario nao ativado

--- src/main/java/br/com/renanrramos/easyshopping/controller/rest/UserController.java
+++ src/main/java/br/com/renanrramos/easyshopping/controller/rest/UserController.java
@@ -77,8 +77,8 @@ public class UserController {
 			return new ResponseEntity<>(response, responseHeaders, HttpStatus.OK);
 		}
 
-		return ResponseEntity.status(HttpStatus.NOT_FOUND)
-				.body("{\"message\":\"" + ExceptionMessagesConstants.USER_NOT_FOUND + "\"}");
+		return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+				.body("{\"message\":\"" + ExceptionMessagesConstants.UNACTIVATED_USER + "\"}");
 	}
 
 	@PostMapping(path = "/logout")
