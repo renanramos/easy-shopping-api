279965abbf96646dd0528a560961eb574101fdfc
Renan Ramos <renan.ramos@inatel.br>
2020-09-15 22:18:20 -0300

    Enum que sera utilizado na implementacao Oauth

---
+++ src/main/java/br/com/renanrramos/easyshopping/config/social/AppProperties.java
@@ -0,0 +1,65 @@
+/**------------------------------------------------------------
+ * Project: easy-shopping
+ * 
+ * Creator: renan.ramos - 13/09/2020
+ * ------------------------------------------------------------
+ */
+package br.com.renanrramos.easyshopping.config.social;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * @author renan.ramos
+ *
+ */
+//@ConfigurationProperties(prefix = "app")
+public class AppProperties {
+
+	private Auth auth = new Auth();
+	private OAuth2 oauth2 = new OAuth2();
+
+	public static class Auth {
+		private String tokenSecret;
+		private long tokenExpirationMsec;
+
+		public String getTokenSecret() {
+            return tokenSecret;
+        }
+
+        public void setTokenSecret(String tokenSecret) {
+            this.tokenSecret = tokenSecret;
+        }
+
+        public long getTokenExpirationMsec() {
+            return tokenExpirationMsec;
+        }
+
+        public void setTokenExpirationMsec(long tokenExpirationMsec) {
+            this.tokenExpirationMsec = tokenExpirationMsec;
+        }
+	}
+
+	public static final class OAuth2 {
+        private List<String> authorizedRedirectUris = new ArrayList<>();
+
+        public List<String> getAuthorizedRedirectUris() {
+            return authorizedRedirectUris;
+        }
+
+        public OAuth2 authorizedRedirectUris(List<String> authorizedRedirectUris) {
+            this.authorizedRedirectUris = authorizedRedirectUris;
+            return this;
+        }
+    }
+
+	public Auth getAuth() {
+        return auth;
+    }
+
+    public OAuth2 getOauth2() {
+        return oauth2;
+    }
+}
