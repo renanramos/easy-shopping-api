aefa69baf170dcd1a04b4e463c5601b3303a2350
Renan Ramos <renan.ramos@inatel.br>
2020-09-15 22:16:17 -0300

    Arquivo com constants de paginacao

---
+++ src/main/java/br/com/renanrramos/easyshopping/constants/messages/ConstantsValues.java
@@ -0,0 +1,22 @@
+/**------------------------------------------------------------
+ * Project: easy-shopping
+ * 
+ * Creator: renan.ramos - 15/09/2020
+ * ------------------------------------------------------------
+ */
+package br.com.renanrramos.easyshopping.constants.messages;
+
+/**
+ * @author renan.ramos
+ *
+ */
+public class ConstantsValues {
+
+	public static final String DEFAULT_PAGE_SIZE = "20";
+	public static final String DEFAULT_PAGE_NUMBER = "0";
+	public static final String DEFAULT_SORT_VALUE = "id";
+
+	private ConstantsValues() {
+		// Intentionally empty
+	}
+}
