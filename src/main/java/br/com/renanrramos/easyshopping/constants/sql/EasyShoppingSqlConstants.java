63cf8435320a19f63878fc70c42dd0a34802f523
Renan Ramos <renan.ramos@inatel.br>
2020-09-23 13:46:18 -0300

    Consulta de empresa por nome

--- src/main/java/br/com/renanrramos/easyshopping/constants/sql/EasyShoppingSqlConstants.java
+++ src/main/java/br/com/renanrramos/easyshopping/constants/sql/EasyShoppingSqlConstants.java
@@ -17,5 +17,7 @@ public class EasyShoppingSqlConstants {
 	public static final String GET_SUBCATEGORIES_BY_NAME = "SELECT s FROM Subcategory s LEFT JOIN ProductCategory p ON p.id = s.productCategory.id WHERE s.name LIKE %:name% or p.name LIKE %:name%";
 
 	public static final String GET_CUSTOMER_BY_NAME = "SELECT c FROM Customer c WHERE (c.name LIKE %:name% or c.cpf LIKE %:name% or c.email LIKE %:name%)";
+
+	public static final String GET_COMPANY_BY_NAME = "SELECT c FROM Company c WHERE (c.name LIKE %:name% or c.registeredNumber LIKE %:name% or c.email LIKE %:name%)";
 	
 }
