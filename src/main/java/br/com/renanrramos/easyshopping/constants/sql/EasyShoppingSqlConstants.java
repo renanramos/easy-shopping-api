3996eba9029c9d0fbd3b05f07a610992fab7f701
Renan Ramos <renan.ramos@inatel.br>
2020-09-23 13:29:27 -0300

    Constante para consulta de subcategorias por nome

--- src/main/java/br/com/renanrramos/easyshopping/constants/sql/EasyShoppingSqlConstants.java
+++ src/main/java/br/com/renanrramos/easyshopping/constants/sql/EasyShoppingSqlConstants.java
@@ -13,5 +13,7 @@ package br.com.renanrramos.easyshopping.constants.sql;
 public class EasyShoppingSqlConstants {
 
 	public static final String GET_CUSTOMER_BY_ID = "FROM Customer c WHERE c.id = :customerId";
+
+	public static final String GET_SUBCATEGORIES_BY_NAME = "SELECT s FROM Subcategory s LEFT JOIN ProductCategory p ON p.id = s.productCategory.id WHERE s.name LIKE %:name% or p.name LIKE %:name%";
 	
 }
