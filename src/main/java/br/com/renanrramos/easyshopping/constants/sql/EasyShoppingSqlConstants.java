6067d081d94976c45d46ebe61a14f175c94c078d
Renan Ramos <renan.ramos@inatel.br>
2020-09-23 14:01:33 -0300

    Constante de consulta de loja por nome, nome fantasia, cnpj e nome da empresa

--- src/main/java/br/com/renanrramos/easyshopping/constants/sql/EasyShoppingSqlConstants.java
+++ src/main/java/br/com/renanrramos/easyshopping/constants/sql/EasyShoppingSqlConstants.java
@@ -20,6 +20,8 @@ public class EasyShoppingSqlConstants {
 
 	public static final String GET_COMPANY_BY_NAME = "SELECT c FROM Company c WHERE (c.name LIKE %:name% or c.registeredNumber LIKE %:name% or c.email LIKE %:name%)";
 
+	public static final String GET_STORE_BY_NAME = "SELECT s FROM Store s LEFT JOIN Company c ON c.id = s.company.id WHERE s.name LIKE %:name% or s.registeredNumber LIKE %:name% or s.corporateName LIKE %:name% or c.name LIKE %:name%";
+
 	private EasyShoppingSqlConstants() {
 		// Intentionally empty
 	}
