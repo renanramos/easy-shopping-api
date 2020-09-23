6b766175b24d6008aa1aae0c9308e0042e4e572f
Renan Ramos <renan.ramos@inatel.br>
2020-09-23 13:50:14 -0300

    Construtor privado

--- src/main/java/br/com/renanrramos/easyshopping/constants/sql/EasyShoppingSqlConstants.java
+++ src/main/java/br/com/renanrramos/easyshopping/constants/sql/EasyShoppingSqlConstants.java
@@ -19,5 +19,8 @@ public class EasyShoppingSqlConstants {
 	public static final String GET_CUSTOMER_BY_NAME = "SELECT c FROM Customer c WHERE (c.name LIKE %:name% or c.cpf LIKE %:name% or c.email LIKE %:name%)";
 
 	public static final String GET_COMPANY_BY_NAME = "SELECT c FROM Company c WHERE (c.name LIKE %:name% or c.registeredNumber LIKE %:name% or c.email LIKE %:name%)";
-	
+
+	private EasyShoppingSqlConstants() {
+		// Intentionally empty
+	}
 }
