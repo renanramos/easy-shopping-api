edbebab8c975cd3cf777d8a10ee500f2a48385a7
Renan Ramos <renan.ramos@inatel.br>
2020-09-23 23:28:22 -0300

    Implementacao das operacoes principais de ProductImageService

---
+++ src/main/java/br/com/renanrramos/easyshopping/service/impl/ProductImageService.java
@@ -0,0 +1,60 @@
+/**------------------------------------------------------------
+ * Project: easy-shopping
+ * 
+ * Creator: renan.ramos - 23/09/2020
+ * ------------------------------------------------------------
+ */
+package br.com.renanrramos.easyshopping.service.impl;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import br.com.renanrramos.easyshopping.model.ProductImage;
+import br.com.renanrramos.easyshopping.repository.ProductImageRepository;
+import br.com.renanrramos.easyshopping.service.CommonService;
+
+/**
+ * @author renan.ramos
+ *
+ */
+@Service
+public class ProductImageService implements CommonService<ProductImage>{
+
+	@Autowired
+	private ProductImageRepository productImageRepository;
+	
+	@Override
+	public ProductImage save(ProductImage productImage) {
+		return productImageRepository.save(productImage);
+	}
+
+	@Override
+	public List<ProductImage> findAll(Pageable page) {
+		return productImageRepository.findAll(page).getContent();
+	}
+
+	@Override
+	public Optional<ProductImage> findById(Long productImageId) {
+		return productImageRepository.findById(productImageId);
+	}
+
+	@Override
+	public ProductImage update(ProductImage productImage) {
+		return productImageRepository.save(productImage);
+	}
+
+	@Override
+	public void remove(Long productImageId) {
+		productImageRepository.deleteById(productImageId);
+	}
+
+	@Override
+	public List<ProductImage> findAllPageable(Pageable page, Long id) {
+		return productImageRepository.findAll(page).getContent();
+	}
+
+}
