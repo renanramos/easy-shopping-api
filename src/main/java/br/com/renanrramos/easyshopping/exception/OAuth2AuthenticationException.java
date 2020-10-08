afae0a70038d315ec6b568ed922f29a52c86a6ca
Renan Ramos <renan.ramos@inatel.br>
2020-10-08 08:28:39 -0300

    Classe para tratamento de excecao por autenticacao

---
+++ src/main/java/br/com/renanrramos/easyshopping/exception/OAuth2AuthenticationException.java
@@ -0,0 +1,26 @@
+/**------------------------------------------------------------
+ * Project: easy-shopping
+ * 
+ * Creator: renan.ramos - 30/09/2020
+ * ------------------------------------------------------------
+ */
+package br.com.renanrramos.easyshopping.exception;
+
+import org.springframework.security.core.AuthenticationException;
+
+/**
+ * @author renan.ramos
+ *
+ */
+public class OAuth2AuthenticationException extends AuthenticationException{
+
+	private static final long serialVersionUID = -5629221218770830818L;
+
+	public OAuth2AuthenticationException(String msg, Throwable t) {
+		super(msg, t);
+	}
+
+	public OAuth2AuthenticationException(String msg) {
+		super(msg);
+	}
+}
