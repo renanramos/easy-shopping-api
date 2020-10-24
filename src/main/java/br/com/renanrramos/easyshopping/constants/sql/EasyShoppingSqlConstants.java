cd9e6da81435b804113376a2e131d30cd0c41fa7
Renan Ramos <renan.ramos@inatel.br>
2020-10-24 01:20:05 -0300

    Atualizacao de codigo sql

--- src/main/java/br/com/renanrramos/easyshopping/constants/sql/EasyShoppingSqlConstants.java
+++ src/main/java/br/com/renanrramos/easyshopping/constants/sql/EasyShoppingSqlConstants.java
@@ -20,9 +20,9 @@ public class EasyShoppingSqlConstants {
 
 	public static final String GET_COMPANY_BY_NAME = "SELECT c FROM Company c WHERE (c.name LIKE %:name% or c.registeredNumber LIKE %:name% or c.email LIKE %:name%)";
 
-	public static final String GET_STORE_BY_NAME = "SELECT s FROM Store s LEFT JOIN Company c ON c.id = s.company.id WHERE s.name LIKE %:name% or s.registeredNumber LIKE %:name% or s.corporateName LIKE %:name% or c.name LIKE %:name%";
+	public static final String GET_STORE_BY_NAME = "SELECT s FROM Store s LEFT JOIN Company c ON c.tokenId = s.companyId WHERE s.name LIKE %:name% or s.registeredNumber LIKE %:name% or s.corporateName LIKE %:name% or c.name LIKE %:name%";
 
-	public static final String GET_STORE_WITH_COMPANY_ID = "SELECT s FROM Store s LEFT JOIN Company c ON c.id = s.company.id WHERE (:name is null OR s.name LIKE %:name%  OR s.registeredNumber LIKE %:name% OR s.corporateName LIKE %:name% OR c.name LIKE %:name%) AND s.company.id = :companyId";
+	public static final String GET_STORE_WITH_COMPANY_ID = "SELECT s FROM Store s LEFT JOIN Company c ON c.tokenId = s.companyId WHERE (:name is null OR s.name LIKE %:name%  OR s.registeredNumber LIKE %:name% OR s.corporateName LIKE %:name% OR c.name LIKE %:name%) AND s.companyId = :tokenId";
 
 	private EasyShoppingSqlConstants() {
 		// Intentionally empty
