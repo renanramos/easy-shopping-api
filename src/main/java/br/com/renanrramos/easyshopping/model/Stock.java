c566d20d2e761a59eb326fe9f9a0fb43fbf53bfb
Renan Ramos <renan.ramos@inatel.br>
2020-11-10 14:46:53 -0300

    Implementacao da entidade Stock

---
+++ src/main/java/br/com/renanrramos/easyshopping/model/Stock.java
@@ -0,0 +1,62 @@
+/**------------------------------------------------------------
+ * Project: easy-shopping
+ *
+ * Creator: renan.ramos - 10/11/2020
+ * ------------------------------------------------------------
+ */
+package br.com.renanrramos.easyshopping.model;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.validation.constraints.NotBlank;
+
+import br.com.renanrramos.easyshopping.constants.messages.ValidationMessagesConstants;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * @author renan.ramos
+ *
+ */
+@Getter
+@Setter
+@Entity
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Stock implements Serializable {
+
+	private static final long serialVersionUID = -1868754634925172817L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@ApiModelProperty(hidden = true)
+	private Long id;
+
+	@Column(nullable = false, length = 50)
+	@NotBlank(message = ValidationMessagesConstants.EMPTY_FIELD)
+	private String name;
+
+	@ManyToOne
+	@JoinColumn(name = "store_id")
+	private Store store;
+
+	@OneToMany(cascade = CascadeType.ALL, mappedBy = "stock")
+	private List<StockItem> items = new ArrayList<>();
+
+	@Override
+	public String toString() {
+		return "Stock [id=" + id + ", name=" + name + ", store=" + store.getName() + "]";
+	}
+}
