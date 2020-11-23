4abfe975551aa5bb1414c313c9bffa82e5ee6534
Renan Ramos <renan.ramos@inatel.br>
2020-11-23 07:06:26 -0300

    Implementacao de entidades para pedido e item do pedido

---
+++ src/main/java/br/com/renanrramos/easyshopping/model/Order.java
@@ -0,0 +1,56 @@
+/**------------------------------------------------------------
+ * Project: easy-shopping
+ *
+ * Creator: renan.ramos - 23/11/2020
+ * ------------------------------------------------------------
+ */
+package br.com.renanrramos.easyshopping.model;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * @author renan.ramos
+ *
+ */
+@Getter
+@Setter
+@Entity
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Order implements Serializable {
+
+	private static final long serialVersionUID = -6015194357896415019L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@ApiModelProperty(hidden = true)
+	private Long id;
+
+	private String customerId;
+
+	@OneToMany(targetEntity = OrderItem.class, cascade = CascadeType.ALL, mappedBy = "order", fetch = FetchType.EAGER)
+	private List<OrderItem> items = new ArrayList<>();
+
+	public Order() {
+		// Intentionally empty
+	}
+
+	@Override
+	public String toString() {
+		return "Order [id=" + id + ", customerId=" + customerId + "]";
+	}
+}
---
+++ src/main/java/br/com/renanrramos/easyshopping/model/OrderItem.java
@@ -0,0 +1,64 @@
+/**------------------------------------------------------------
+ * Project: easy-shopping
+ *
+ * Creator: renan.ramos - 23/11/2020
+ * ------------------------------------------------------------
+ */
+package br.com.renanrramos.easyshopping.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * @author renan.ramos
+ *
+ */
+@Getter
+@Setter
+@Entity
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class OrderItem implements Serializable {
+
+	private static final long serialVersionUID = 6973936402754269388L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@ApiModelProperty(hidden = true)
+	private Long id;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "order_id")
+	@Fetch(FetchMode.JOIN)
+	@JsonIgnore
+	private Order order;
+
+	private Long productId;
+
+	private Double amount;
+
+	private Double unitValue;
+
+	private Double total;
+
+	public OrderItem() {
+		// Intentionally empty
+	}
+
+}
