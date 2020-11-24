f61a94757ba080244509d0dea200b49cd49e6c39
Renan Ramos <renan.ramos@inatel.br>
2020-11-23 21:19:39 -0300

    Classe e interface para obter as informacoes do usuario logado

---
+++ src/main/java/br/com/renanrramos/easyshopping/service/AuthenticationService.java
@@ -0,0 +1,18 @@
+/**------------------------------------------------------------
+ * Project: easy-shopping
+ *
+ * Creator: renan.ramos - 23/11/2020
+ * ------------------------------------------------------------
+ */
+package br.com.renanrramos.easyshopping.service;
+
+import org.springframework.security.core.Authentication;
+
+/**
+ * @author renan.ramos
+ *
+ */
+public interface AuthenticationService {
+
+	Authentication getAuthentication();
+}
