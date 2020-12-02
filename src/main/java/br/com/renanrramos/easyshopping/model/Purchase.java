044ecf6739136de27796ff96a22ecb0023ac04d5
Renan Ramos <renan.ramos@inatel.br>
2020-12-02 18:08:34 -0300

    Nova entidade purchase

---
+++ src/main/java/br/com/renanrramos/easyshopping/model/Purchase.java
@@ -0,0 +1,67 @@
+/**------------------------------------------------------------
+ * Project: easy-shopping
+ *
+ * Creator: renan.ramos - 02/12/2020
+ * ------------------------------------------------------------
+ */
+package br.com.renanrramos.easyshopping.model;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * @author renan.ramos
+ *
+ */
+@Getter
+@Setter
+@Entity
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Purchase implements Serializable {
+
+	private static final long serialVersionUID = -6564412536666152947L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@ApiModelProperty(hidden = true)
+	private Long id;
+
+	private String customerId;
+
+	@OneToOne(mappedBy = "purchase", cascade = CascadeType.ALL, fetch = FetchType.LAZY, optional = false)
+	private Order order;
+
+	@OneToOne(mappedBy = "purchase", cascade = CascadeType.ALL, fetch = FetchType.LAZY, optional = false)
+	private Address address;
+
+	@OneToOne(mappedBy = "purchase", cascade = CascadeType.ALL, fetch = FetchType.LAZY, optional = false)
+	private CreditCard creditCard;
+
+	@DateTimeFormat(pattern = "yyyy-MM-dd HH:mm")
+	private LocalDateTime date;
+
+	public Purchase() {
+		// Intentionally empty
+	}
+
+	@Override
+	public String toString() {
+		return "Purchase [id=" + id + ", customerId=" + customerId + ", order=" + order + ", address=" + address
+				+ ", creditCard=" + creditCard + ", date=" + date + "]";
+	}
+}
