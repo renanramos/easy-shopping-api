373d3da829ee6afe2b8497e206d347c98415f035
Renan Rodrigues Ramos <renan.ramos@inatel.br>
2020-12-28 15:05:17 -0300

    Metodo para efetuar a remocao de imagem da tabea ProductImage

--- src/main/java/br/com/renanrramos/easyshopping/repository/ProductImageRepository.java
+++ src/main/java/br/com/renanrramos/easyshopping/repository/ProductImageRepository.java
@@ -6,8 +6,14 @@
  */
 package br.com.renanrramos.easyshopping.repository;
 
+import java.util.Optional;
+
+import javax.transaction.Transactional;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 import br.com.renanrramos.easyshopping.model.ProductImage;
@@ -19,4 +25,10 @@ import br.com.renanrramos.easyshopping.model.ProductImage;
 public interface ProductImageRepository extends PagingAndSortingRepository<ProductImage, Long> {
 
 	Page<ProductImage> findProductImageByProductId(Pageable page, Long productId);
+	Optional<ProductImage> findTopProductImageByProductId(Long productId);
+
+	@Transactional
+	@Modifying
+	@Query("DELETE FROM ProductImage WHERE ID = :productImageId")
+	public void removeById(Long productImageId);
 }
