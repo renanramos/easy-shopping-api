a178c9b76f7111be72b967100b4f64095b8b7191
Renan Rodrigues Ramos <renanramos@ciandt.com>
2024-09-09 22:19:36 -0300

    Mappers para ProductImage e Purchase

--- src/main/java/br/com/renanrramos/easyshopping/model/dto/PurchaseDTO.java
+++ src/main/java/br/com/renanrramos/easyshopping/model/dto/PurchaseDTO.java
@@ -7,20 +7,17 @@
 package br.com.renanrramos.easyshopping.model.dto;
 
 import java.time.LocalDateTime;
-import java.util.List;
-import java.util.stream.Collectors;
 
-import br.com.renanrramos.easyshopping.model.Purchase;
+import lombok.Data;
+import lombok.ToString;
 import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.Setter;
 
 /**
  * @author renan.ramos
  *
  */
-@Getter
-@Setter
+@Data
+@ToString
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class PurchaseDTO {
 
@@ -38,28 +35,5 @@ public class PurchaseDTO {
 
 	public PurchaseDTO() {
 		// Intentionally empty
-	}
-
-	public PurchaseDTO(Purchase purchase) {
-		this.id = purchase.getId();
-		this.customerId = purchase.getCustomerId();
-		this.orderId = purchase.getOrder().getId();
-		this.addressId = purchase.getAddress().getId();
-		this.creditCardId = purchase.getCreditCard().getId();
-		this.date = purchase.getPurchaseDate();
-	}
-
-	public static List<PurchaseDTO> convertPurchaseListToPurchaseDTOList(List<Purchase> purchases) {
-		return purchases.stream().map(PurchaseDTO::new).collect(Collectors.toList());
-	}
-
-	public static PurchaseDTO convertPurchaseToPurchaseDTO(Purchase purchase) {
-		return new PurchaseDTO(purchase);
-	}
-
-	@Override
-	public String toString() {
-		return "PurchaseDTO [id=" + id + ", customerId=" + customerId + ", order=" + orderId + ", address=" + addressId
-				+ ", creditCard=" + creditCardId + ", date=" + date + "]";
 	}
 }
